3c8b174e61d3d99a4072e7f1f73c402696a00aef
Marcel <me@m4rc3l.de>
2024-06-26 20:23:52 +0200

    router: convert path to query param

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,11 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    provideHttpClient()
+    provideRouter([]),
+    provideHttpClient(),
   ]
 };
