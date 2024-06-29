202156fb15bfa641bd67b4df73229af5bb40aa63
Sandro Jäckel <sandro.jaeckel@gmail.com>
2024-06-29 21:12:33 +0200

    Allow using options.json directly

--- nix/wrapper.nix
+++ nix/wrapper.nix
@@ -11,7 +11,7 @@ rec {
       warningsAreErrors = false;
     }).optionsJSON + /share/doc/nixos/options.json;
 
-  mkSearchJSON = listOfModuleAndUrlPrefix:
+  mkSearchJSON = searchArgs:
     runCommand "options.json"
       { nativeBuildInputs = [ (python3.withPackages (ps: with ps; [ markdown pygments ])) ]; }
       (''
@@ -19,26 +19,36 @@ rec {
         python \
           ${./fixup-options.py} \
       '' + lib.concatStringsSep " " (lib.flatten (map (opt: [
-        (mkOptionsJSON opt.modules) "'${opt.urlPrefix}'"
-        ]) listOfModuleAndUrlPrefix)) + ''
+        (opt.optionsJSON or mkOptionsJSON opt.modules) "'${opt.urlPrefix}'"
+        ]) searchArgs)) + ''
           > $out/options.json
       '');
 
-  mkSearch = { modules, urlPrefix }:
+  mkSearch = { modules ? null, optionsJSON ? null, urlPrefix }:
+    let
+      args = {
+        inherit urlPrefix;
+      } // lib.optionalAttrs (modules != null) modules
+        // lib.optionalAttrs (optionsJSON != null) optionsJSON;
+    in
     runCommand "nuscht-search"
       { nativeBuildInputs = [ xorg.lndir ]; }
       ''
         mkdir $out
         lndir ${nuscht-search} $out
-        ln -s ${mkSearchJSON [ { inherit modules urlPrefix; } ]} $out/options.json
+        ln -s ${mkSearchJSON [ args ]} $out/options.json
       '';
 
-  mkMultiSearch = listOfModuleAndUrlPrefix:
+  # mkMultiSearch [
+  #   { modules = [ self.inputs.nixos-modules.nixosModule ]; urlPrefix = "https://github.com/NuschtOS/nixos-modules/blob/main/"; }
+  #   { optionsJSON = ./path/to/options.json; urlPrefix = "https://git.example.com/blob/main/"; }
+  # ]
+  mkMultiSearch = searchArgs:
     runCommand "nuscht-search"
       { nativeBuildInputs = [ xorg.lndir ]; }
       ''
         mkdir $out
         lndir ${nuscht-search} $out
-        ln -s ${mkSearchJSON listOfModuleAndUrlPrefix}/options.json $out/options.json
+        ln -s ${mkSearchJSON searchArgs}/options.json $out/options.json
       '';
 }
