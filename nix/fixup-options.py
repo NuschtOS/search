cfde3f621aeef68405d35491841afda8d9731a85
Marcel <34819524+MarcelCoding@users.noreply.github.com>
2024-07-08 23:47:35 +0200

    options: sanitize html (#50)

--- nix/fixup-options.py
+++ nix/fixup-options.py
@@ -5,6 +5,9 @@ import markdown
 from pygments import highlight
 from pygments.lexers import NixLexer
 from pygments.formatters import HtmlFormatter
+from html_sanitizer import Sanitizer
+
+sanitizer = Sanitizer()
 
 if (len(sys.argv) - 1) % 2 != 0:
   print("Usage: path/to/options.json https://example.com/option/ path/to/other/options.json https://example.com/other_option/")
@@ -14,7 +17,7 @@ def code(code):
   if code["_type"] == 'literalExpression':
     return highlight(code["text"], NixLexer(), HtmlFormatter())
   elif code["_type"] == 'literalMD':
-    return markdown.markdown(code["text"])
+    return sanitizer.sanitize(markdown.markdown(code["text"]))
   else:
     print("ERROR: cannot handle a " + code["_type"], file=sys.stderr)
     sys.exit(1)
@@ -41,7 +44,7 @@ for i in range(1,len(sys.argv),2):
       del entry["loc"]
       entry["declarations"] = list(map(lambda x: update_declaration(url, x), entry["declarations"]))
 
-      entry["description"] = markdown.markdown(entry["description"])
+      entry["description"] = sanitizer.sanitize(markdown.markdown(entry["description"]))
       if 'default' in entry:
         entry['default'] = code(entry["default"])
       if 'example' in entry:
