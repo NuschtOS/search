2bcdba137ec5769f63f95446c506bda923b4ab00
Marcel <me@m4rc3l.de>
2024-09-03 21:51:39 +0200

    gh-actions: build nixos stable and unstable

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
