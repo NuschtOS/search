7a0d38df8c14e906c8863aa0a8dbdf536c4ce889
Marcel <me@m4rc3l.de>
2024-10-08 23:15:35 +0200

    title: add option

--- nix/wrapper.nix
+++ nix/wrapper.nix
@@ -40,26 +40,27 @@ rec {
 
   # mkMultiSearch {
   #   baseHref = "/search/";
+  #   title = "Custom Search";
   #   scopes = [
   #     { modules = [ self.inputs.nixos-modules.nixosModule ]; urlPrefix = "https://github.com/NuschtOS/nixos-modules/blob/main/"; }
   #     { optionsJSON = ./path/to/options.json; optionsPrefix = "programs.example"; urlPrefix = "https://git.example.com/blob/main/"; }
   #   ];
   # };
-  mkMultiSearch = { scopes, baseHref ? "/" }:
+  mkMultiSearch = { scopes, baseHref ? "/", title ? "NüschtOS Search" }:
     runCommand "nuscht-search"
       { nativeBuildInputs = [ xorg.lndir ]; }
       ''
         mkdir $out
-        lndir ${nuscht-search.override { inherit baseHref; }} $out
+        lndir ${nuscht-search.override { inherit baseHref title; }} $out
         ln -s ${mkSearchJSON scopes}/options.json $out/options.json
       '';
 
   # mkSearch { modules = [ self.inputs.nixos-modules.nixosModule ]; urlPrefix = "https://github.com/NuschtOS/nixos-modules/blob/main/"; }
   # mkSearch { optionsJSON = ./path/to/options.json; optionsPrefix = "programs.example"; urlPrefix = "https://git.example.com/blob/main/"; }
-  # mkSearch { optionsJSON = ./path/to/options.json; urlPrefix = "https://git.example.com/blob/main/"; baseHref = "/search/"; }
-  mkSearch = { baseHref ? "/", ... }@args:
+  # mkSearch { optionsJSON = ./path/to/options.json; urlPrefix = "https://git.example.com/blob/main/"; baseHref = "/search/"; title = "Custom Search"; }
+  mkSearch = { baseHref ? "/", title ? "NüschtOS Search", ... }@args:
     mkMultiSearch {
-      inherit baseHref;
-      scopes = [ (lib.removeAttrs args [ "baseHref" ]) ];
+      inherit baseHref title;
+      scopes = [ (lib.removeAttrs args [ "baseHref" "title" ]) ];
     };
 }
