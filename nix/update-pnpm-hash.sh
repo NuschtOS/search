3a67b626c9d1e9bf1f03fe9a6a53af6c8e223fc2
Sandro <sandro.jaeckel@gmail.com>
2024-10-10 00:19:26 +0200

    Fix update script #2 (#100)

--- nix/update-pnpm-hash.sh
+++ nix/update-pnpm-hash.sh
@@ -4,5 +4,5 @@ cd "$(dirname "${BASH_SOURCE[0]}")" || exit 1
 fake_hash="sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="
 old_hash="$(command nix eval --raw ..#nuscht-search.pnpmDeps.outputHash)"
 sed -i "s|$old_hash|$fake_hash|" frontend.nix
-new_hash="$(command nix build ..#nuscht-search.pnpmDeps |& grep "got:" | cut -d':' -f2 | sed 's| ||g')"
+new_hash="$({ command nix build ..#nuscht-search.pnpmDeps || true; } |& grep "got:" | cut -d':' -f2 | sed 's| ||g')"
 sed -i "s|$fake_hash|$new_hash|" frontend.nix
