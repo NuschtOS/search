d67924960418f5e54b37b9f87e38341c560c5ee3
Marcel <me@m4rc3l.de>
2024-10-10 21:55:08 +0200

    ixx: init

--- src/app/core/components/search/search.component.ts
+++ src/app/core/components/search/search.component.ts
@@ -1,7 +1,7 @@
 import { AfterViewInit, ChangeDetectionStrategy, Component } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
-import { Observable, map, switchMap } from 'rxjs';
-import { SearchService, Option } from '../../data/search.service';
+import { Observable, map, of, switchMap } from 'rxjs';
+import { MAX_SEARCH_RESULTS, SearchService, SearchedOption } from '../../data/search.service';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { TextFieldComponent } from "@feel/form";
 import { AsyncPipe, NgFor, NgIf } from '@angular/common';
@@ -24,15 +24,15 @@ export class SearchComponent implements AfterViewInit {
   protected readonly results = this.search.valueChanges.pipe(
     switchMap(query => {
       const q = query?.trim();
-      if (q !== getQuery()) this.router.navigate([], { queryParams: { query: q }, queryParamsHandling: 'merge' });
-      if (q && q.length > 0) {
-        return this.searchService.search(q);
-      } else {
-        return this.searchService.all();
+      if (q !== getQuery()) {
+        this.router.navigate([], { queryParams: { query: q }, queryParamsHandling: 'merge' });
       }
+      return this.searchService.search(q);
     }),
   );
+
   protected readonly selectedOption = this.activatedRoute.queryParams.pipe(map(({ option }) => option));
+  protected readonly maxSearchResults = MAX_SEARCH_RESULTS;
 
   constructor(
     private readonly searchService: SearchService,
@@ -45,12 +45,12 @@ export class SearchComponent implements AfterViewInit {
     this.search.setValue(getQuery());
   }
 
-  protected trackBy(_idx: number, { name }: Option): string {
-    return name;
+  protected trackBy(_idx: number, option: SearchedOption): number {
+    return option.idx;
   }
 
-  protected isActive({ name }: Option): Observable<boolean> {
-    return this.selectedOption.pipe(map(option => option === name));
+  protected isActive(opt: SearchedOption): Observable<boolean> {
+    return this.selectedOption.pipe(map(option => option === opt.name));
   }
 }
 
