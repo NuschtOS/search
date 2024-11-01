0fd180898532528b7dfdc161eeebb5837d187e44
SuperSandroBot <sandro.jaeckel+bot@gmail.com>
2024-11-01 20:53:12 +0100

    Update angular-cli monorepo to v18.2.11

--- nix/frontend.nix
+++ nix/frontend.nix
@@ -25,7 +25,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   pnpmDeps = pnpm.fetchDeps {
     inherit (finalAttrs) pname version src;
-    hash = "sha256-4ri74PmqwWvuHTgrk3nnP/+RgBfl8GzA1mwBR1VJjTk=";
+    hash = "sha256-Bl+K07IXv1DfWMg5cKKSRolPWCnLBSGjwq1ijqjv+Tk=";
   };
 
   nativeBuildInputs = [ nodejs pnpm.configHook ];
