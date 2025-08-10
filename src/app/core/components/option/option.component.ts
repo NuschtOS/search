44a181bb1d8aad3b7f40d3cad9fc280b549b70b5
Marcel <34819524+MarcelCoding@users.noreply.github.com>
2025-08-10 02:29:52 +0200

    option: add loading indicator (#239)

--- src/app/core/components/option/option.component.ts
+++ src/app/core/components/option/option.component.ts
@@ -1,20 +1,21 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
-import { switchMap, map } from 'rxjs';
+import { switchMap, map, merge, of, BehaviorSubject, tap } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
 import { SearchService } from '../../data/search.service';
+import { LoadingIndicatorComponent } from "../loading-indicator/loading-indicator.component";
 
 @Component({
   selector: 'app-option',
-  imports: [AsyncPipe, RouterLink],
+  imports: [AsyncPipe, RouterLink, LoadingIndicatorComponent],
   templateUrl: './option.component.html',
   styleUrl: './option.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class OptionComponent {
 
+  protected readonly loading = new BehaviorSubject(false);
   protected readonly option;
-
   protected readonly scope;
 
   constructor(
@@ -22,11 +23,13 @@ export class OptionComponent {
     private readonly searchService: SearchService,
   ) {
     this.option = this.activatedRoute.queryParams.pipe(
-      switchMap(({ option_scope, option }) => this.searchService.getByName(Number(option_scope), option)),
+      tap(() => this.loading.next(true)),
+      switchMap(({ option_scope, option }) => merge(of(null), this.searchService.getByName(Number(option_scope), option))),
+      tap(() => this.loading.next(false)),
     );
 
     this.scope = this.activatedRoute.queryParams.pipe(
-      switchMap(({ option_scope }) => this.searchService.getScopes().pipe(map(scopes => scopes[Number(option_scope)]))),
+      switchMap(({ option_scope }) => merge(of(null), this.searchService.getScopes().pipe(map(scopes => scopes[Number(option_scope)])))),
     );
   }
 }
