a5846adc7a3cfbb906379174fea4f851af53ac38
Sandro Jäckel <sandro.jaeckel@gmail.com>
2025-08-11 00:03:16 +0200

    Bump feel to fix double highlight in text input

--- nix/update-pnpm-hash.sh
+++ nix/update-pnpm-hash.sh
@@ -2,7 +2,7 @@
 set -eou pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")" || exit 1
 fake_hash="sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="
-old_hash="$(command nix eval --raw ..#nuscht-search.pnpmDeps.outputHash)"
+old_hash="$(command nix eval --raw ..#nixpkgs-search.pnpmDeps.outputHash)"
 sed -i "s|$old_hash|$fake_hash|" frontend.nix
-new_hash="$({ command nix build ..#nuscht-search.pnpmDeps || true; } |& grep "got:" | cut -d':' -f2 | sed 's| ||g')"
+new_hash="$({ command nix build ..#nixpkgs-search.pnpmDeps || true; } |& grep "got:" | cut -d':' -f2 | sed 's| ||g')"
 sed -i "s|$fake_hash|$new_hash|" frontend.nix
