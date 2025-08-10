51bc9cef1fb85945b3e8fa36845a1811316c0eef
Sandro Jäckel <sandro.jaeckel@gmail.com>
2025-08-11 00:29:54 +0200

    Bump @types/node

--- nix/frontend.nix
+++ nix/frontend.nix
@@ -34,7 +34,7 @@ stdenv.mkDerivation (finalAttrs: {
   pnpmDeps = pnpm.fetchDeps {
     inherit (finalAttrs) pname version src;
     fetcherVersion = 2;
-    hash = "sha256-24Yrlm1T7QDcXA083tmnh9plcEdV6l2mP6E6hDFloqc=";
+    hash = "sha256-Yj3y9veN3DpzjND1wr9dXilXAJfEHtCq8aQfeuRix0Y=";
   };
 
   nativeBuildInputs = [ nodejs pnpm.configHook ];
