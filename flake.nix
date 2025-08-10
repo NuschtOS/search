aee45105708b145d3ba12bfdc7e77cb2e403e8d6
Sandro Jäckel <sandro.jaeckel@gmail.com>
2025-08-11 00:36:29 +0200

    Bump fixx in flake

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     flake-utils.url = "github:numtide/flake-utils";
     ixx = {
       # match version with npm package
-      url = "github:NuschtOS/ixx/v0.1.0";
+      url = "github:NuschtOS/ixx/v0.1.1";
       inputs = {
         nixpkgs.follows = "nixpkgs";
         flake-utils.follows = "flake-utils";
