581923a01628db7bf187849b16c7173cdc78cf3b
Sandro Jäckel <sandro.jaeckel@gmail.com>
2025-10-29 01:54:46 +0100

    Update other node deps

--- nix/frontend.nix
+++ nix/frontend.nix
@@ -34,7 +34,7 @@ stdenv.mkDerivation (finalAttrs: {
   pnpmDeps = pnpm.fetchDeps {
     inherit (finalAttrs) pname version src;
     fetcherVersion = 2;
-    hash = "sha256-9hH+mDtaSqUOXBM/GVFNP4PM8zqXwCiBSOxNdetmDps=";
+    hash = "sha256-MiGxy4E9tlpUVxUGWwtjwoGeeQw1mmq5NwonkFUZP0Y=";
   };
 
   nativeBuildInputs = [ nodejs pnpm.configHook ];
