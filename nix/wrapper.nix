8cf50719f38852abf8bf12a09eb5e4e7395323cb
Marcel <me@m4rc3l.de>
2025-11-16 03:08:46 +0100

    wrapper: generate smaller batches

--- nix/wrapper.nix
+++ nix/wrapper.nix
@@ -59,7 +59,8 @@ let
         let
           last = builtins.head (lib.sublist (builtins.length name - 1) 1 name);
         in
-        lib.toLower (builtins.substring 0 1 last)
+        # this also works if `last` has only one character
+        lib.toLower (builtins.substring 0 2 last)
       )
       pkgNames;
 in
