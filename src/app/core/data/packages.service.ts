88389ca1934ed0ce116f87c333ebe47d4eee4f11
Sandro Jäckel <sandro.jaeckel@gmail.com>
2025-12-01 03:20:15 +0100

    Fix long description

--- src/app/core/data/packages.service.ts
+++ src/app/core/data/packages.service.ts
@@ -11,7 +11,7 @@ export interface Package {
   purl: string,
   declaration?: string,
   description?: string,
-  long_description?: string,
+  longDescription?: string,
   evalError?: boolean,
   homepages: string[],
   knownVulnerabilities: string[],
