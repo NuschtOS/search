d053ea4114c11110b7af365bcdae739f4adb0b99
Sandro Jäckel <sandro.jaeckel@gmail.com>
2025-12-06 04:18:15 +0100

    Update ixx, bump pnpm

--- nix/frontend.nix
+++ nix/frontend.nix
@@ -7,8 +7,8 @@ let
   # pin pnpm version to avoid hash mismatches with differing pnpm versions
   # on nixos stable and unstable
   pnpm' = callPackage (path + "/pkgs/development/tools/pnpm/generic.nix") {
-    version = "10.18.0";
-    hash = "sha256-OWej7+KQnfMF/sS4M6ME38oXw4C2u3dnL02sTyzdN4g=";
+    version = "10.24.0";
+    hash = "sha256-GW9L0XTry9mXhrM0UvFEyy3DLvTnE47URJHp1D1wLXU=";
   };
   pnpm = pnpm' // {
     passthru = pnpm'.passthru // {
@@ -47,7 +47,7 @@ stdenv.mkDerivation (finalAttrs: {
     inherit (finalAttrs) pname version src;
     fetcherVersion = 2;
     postPatch = cpFixx;
-    hash = "sha256-FzOF9y58z/id/Fz+2JX7YtPWlAv8oC9su24idlPFCCM=";
+    hash = "sha256-Wnh8Nh2aulN5FwFb1yfmENWTkgjjqGTKppfb6PFcLEw=";
   };
 
   nativeBuildInputs = [ nodejs pnpm.configHook ];
