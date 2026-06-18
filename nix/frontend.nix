e1b63dceaf4b5b1d54df48a53bae79f9423f591a
Sandro <sandro.jaeckel@gmail.com>
2026-06-19 00:09:37 +0200

    Dedupe substituteInPlace

--- nix/frontend.nix
+++ nix/frontend.nix
@@ -24,9 +24,7 @@ stdenv.mkDerivation (finalAttrs: {
   strictDeps = true;
 
   postPatch = ''
-    substituteInPlace src/index.html \
-      --replace-fail '##TITLE##' ${lib.escapeShellArg config.title}
-    substituteInPlace public/opensearch-options.xml public/opensearch-packages.xml \
+    substituteInPlace src/index.html public/opensearch-options.xml public/opensearch-packages.xml \
       --replace-fail '##TITLE##' ${lib.escapeShellArg config.title}
 
     # remove development files
